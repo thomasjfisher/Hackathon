65d9c0429bf3f5e3df9d2e0e17b14ed6262ba165
rachel williams <rachel.williams@leapdev.io>
2020-09-21 16:38:44 +1000

    touch ups

--- new.js
+++ new.js
@@ -5,12 +5,28 @@
 
 // let arrowUp = document.getElementById("up");
 
-//STATIC ARROWS
+
+//1. the static buttons in the game div (bottom) 
+// - position bottom and center
+
+//2. Start button
+// - create start btn
+// - let btn = document.querySelector("HTMLBUTTONNAME");
+// - define button on click > target arrows and assign to variable
+
+//3. Game trigger > Arrow
+// -start button to trigger game
+// - button on click > target random arrow element (right/left one at a time) move arrow position down x pixels
+// - define base line div or height from the top (variable let baseline = )
+// - check for keyboard events and compare event value to the arrow (keyboard events - google)
+// - if match = success or else error
+// - ARRAY > randomly select > output arrow
+// - set interval for arrows falling
+// - stop game after 1:00 minute?
+
+// position the arrrow
 
 
-//1. start button to trigger game
-//2. the static buttons in the game div (bottom) > buttons (canvas)
-//3. Generating arrows randomly at the top (in set positions and floating down to their respective arrow)
 
 
 
