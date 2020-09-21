5661de24c3bda74247709897a2c57f8025d5cb46
THOMAS <THOMASJFISHER1@GMAIL.COM>
2020-09-21 12:56:06 +1000

    Added canvas for game

--- new.js
+++ new.js
@@ -1,3 +1,21 @@
 ;(function () {
   console.log('IIFE JS Loaded!')
 })()
+
+
+
+var c = document.getElementById("gameCanvas");
+var ctx = c.getContext("2d");
+// Create gradient
+var grd = ctx.createLinearGradient(0,0,200,0);
+grd.addColorStop(0,"white");
+grd.addColorStop(1,"white");
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10,10,150,80);
+
+
+
+
+
+
